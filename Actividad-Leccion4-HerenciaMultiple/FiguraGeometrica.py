ca3a0122821e03a3669d12773ed07b8132768b80
Jorgealza9206 <jaalzar@udistrital.edu.co>
2024-03-17 21:27:20 -0500

    Laboratorio Herencia multiple parte 1 hecho desde windows

--- Actividad-Leccion4-HerenciaMultiple/FiguraGeometrica.py
+++ Actividad-Leccion4-HerenciaMultiple/FiguraGeometrica.py
@@ -4,22 +4,20 @@ class FiguraGeometrica:
         self._ancho = ancho
 
     @property
-    def get_alto(self):
+    def alto(self):
         return self._alto
 
+    @alto.setter
+    def alto(self, alto):
+        self._alto = alto
+
     @property
-    def get_ancho(self):
+    def ancho(self):
         return self._ancho
 
-''' Tenemos errores frente a los setter por el momento vamos a ejecutar el codigo evitando hacerlos'''
-
-    # @alto.setter
-    # def alto(self, alto):
-    #     self._alto = alto
-    #
-    # @ancho.setter
-    # def ancho(self, ancho):
-    #     self._ancho = ancho
+    @ancho.setter
+    def ancho(self, ancho):
+        self._ancho = ancho
 
     def __str__(self):
         return f'La figura tiene un alto de {self._alto} y un ancho de {self._ancho}'
--- Actividad-Leccion4-HerenciaMultiple/main.py
+++ Actividad-Leccion4-HerenciaMultiple/main.py
@@ -1,11 +1,10 @@
 
 from Cuadrado import *
 
-''' Tenemos errores frente a los setter por el momento vamos a ejecutar el codigo evitando hacerlos'''
-triangulo1 = FiguraGeometrica(3,4)
+#triangulo1 = FiguraGeometrica(3,4)
 #triangulo2 = Color('verde')
-#cuadrado1 = Cuadrado(5,'verde')
-
-print(triangulo1)
+cuadrado1 = Cuadrado(5,'verde')
+#print(triangulo1)
 #print(triangulo2)
-print(cuadrado1.area())
+print(f'La figura tiene un area de {cuadrado1.area()}')
+print(cuadrado1)
