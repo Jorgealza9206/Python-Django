529d8c168df55d2deae3dbfff9cca26726e6c81d
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-01-24 19:11:55 -0500

    Terminando video 223, sección 31. Enero 24 de 2025: Trabajamos con el format para formatear los strings, todos los ejemplos los podemos ver en profundizandoPython

--- profundizandoPython/main.py
+++ profundizandoPython/main.py
@@ -1,1 +1,19 @@
-help(str.capitalize)
+# nombre = 'Jorge'
+# edad = 32
+# mensaje_con_formato = 'Mi nombre es %s y tengo %d años'%(nombre,edad)
+# print(mensaje_con_formato)
+
+# persona = ('Karla', 'Gomez', 5000.00)
+# # mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.f'%persona
+# # print(mensaje_con_formato)
+# mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f'
+# print(mensaje_con_formato%persona)
+
+nombre = 'Jorge'
+edad = 32
+sueldo = 3000000
+# mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje_con_formato)
+
+mensaje = 'Sueldo {2:.2f} Nombre {0} Edad {1} '.format(nombre, edad, sueldo)
+print(mensaje)
