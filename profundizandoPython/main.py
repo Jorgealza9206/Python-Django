a5b5832aa69d8d1c798e19e6cf18325f7d90e7fb
Alza-Ramirez Jorge-Alexander <jorge.alza@inetum.com>
2025-02-04 18:32:04 -0500

    Inetum. Seccion 32, Video 226 finalizado

--- profundizandoPython/main.py
+++ profundizandoPython/main.py
@@ -15,12 +15,20 @@ sueldo = 3000000
 # mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
 # print(mensaje_con_formato)
 
-mensaje = 'Sueldo {2:.2f} Nombre {0} Edad {1} '.format(nombre, edad, sueldo)
+# mensaje = 'Sueldo {2:.2f} Nombre {0} Edad {1} '.format(nombre, edad, sueldo)
+# # print(mensaje)
+#
+# mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo)
+# # print(mensaje)
+#
+# diccionario = {'nombre':'Iván','edad':35, 'sueldo':5000.00}
+# mensaje = 'Nombre {persona[nombre]} Edad {persona[edad]} Sueldo {persona[sueldo]:.2f}'.format(persona=diccionario)
 # print(mensaje)
 
-mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo)
-# print(mensaje)
-
-diccionario = {'nombre':'Iván','edad':35, 'sueldo':5000.00}
-mensaje = 'Nombre {persona[nombre]} Edad {persona[edad]} Sueldo {persona[sueldo]:.2f}'.format(persona=diccionario)
+#f-string
+mensaje = f'Nombre {nombre} Edad {edad} Sueldo {sueldo:.2f}'
 print(mensaje)
+
+print(nombre, edad, sueldo)
+
+#multiplicación de str
