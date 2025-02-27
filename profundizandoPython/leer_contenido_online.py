3c4b0f0cffccc8f8c23e6610a0d8deb18ca873ab
Jorgealza9206 <jaalzar@udistrital.edu.co>
2025-02-26 22:31:30 -0500

    Terminando video 234, sección 33, leer un archivo HTML  directamente de internet y creando un txt con su contenido

--- profundizandoPython/leer_contenido_online.py
+++ profundizandoPython/leer_contenido_online.py
@@ -1,4 +1,5 @@
 #Leer contenido online
+import urllib
 from urllib.request import urlopen
 
 # Debido a cambios en la libreria se deben hacer los siguientes cambios:
@@ -9,7 +10,10 @@ peticion = urllib.request.Request(
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     }
 )
+with urlopen(peticion) as mensaje:
+    # contenido = mensaje.read()
+    contenido = mensaje.read().decode('utf-8')
+    print(contenido)
 
-with urlopen('http://globalmentoring.com.mx/recursos/GlobalMentoring.txt') as mensaje:
-    contenido = mensaje.read()
-    print(mensaje)
+with open('nuevo_archivo.txt','w', encoding='utf-8') as archivo:
+    archivo.write(contenido)
