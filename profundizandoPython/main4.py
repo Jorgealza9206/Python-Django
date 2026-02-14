413637fd20d5708108d15fb7240fe0b89f9719f5
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-02-13 20:37:21 -0500

    Video 238 terminado

--- profundizandoPython/main4.py
+++ profundizandoPython/main4.py
@@ -1,6 +1,13 @@
 mensaje = 'Hola mundo'
-print(mensaje.lower().islower())
-print(mensaje.isupper())
-print(mensaje.upper().isupper())
+# print(mensaje.lower().islower())
+# print(mensaje.isupper())
+# print(mensaje.upper().isupper())
 
-#
+#Alinear cadenas
+
+# center - Centrar un string
+titulo = 'Sitio web de JorgeAlza.com.co'
+#print(len(titulo))
+print(titulo.center(50,'*'))
+print(len(titulo.center(50,'*')))
+print(titulo.center(len(titulo)+10,'-'))
