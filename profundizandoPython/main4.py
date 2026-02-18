989e1302508e61be44c63514dcc3427ee091229e
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-02-18 07:59:11 -0500

    Sección 34. Video 239 finalizado, clase de strip

--- profundizandoPython/main4.py
+++ profundizandoPython/main4.py
@@ -13,8 +13,26 @@ titulo = 'Sitio web de JorgeAlza.com.co'
 # print(titulo.center(len(titulo)+10,'-'))
 
 #ljust -Alinea a la izquierda
-print(titulo.ljust(50,'*'))
-print(titulo.ljust(len(titulo)+10,'-'))
+# print(titulo.ljust(50,'*'))
+# print(titulo.ljust(len(titulo)+10,'-'))
+#
+# #Alinea hacia la derecha
+# print(titulo.rjust(len(titulo)+10,'-'))
 
-#Alinea hacia la derecha
-print(titulo.rjust(len(titulo)+10,'-'))
+#Reemplazar
+print(titulo.replace(' ','-'))
+
+#Eliminar caracteres al inicio y al final
+titulo = ' ***GlobalMentoring.com.mx*** '
+print('Cadena original:', titulo, len(titulo))
+titulo = titulo.strip() #Quita espacios al inicio y al final
+print('Cadena modificada:', titulo, len(titulo))
+titulo = '***GlobalMentoring.com.mx***'.strip('*') #Quita todos los asteriscos a la der e izq
+print('Cadena modificada:', titulo, len(titulo))
+titulo = '***GlobalMentoring.com.mx***'.lstrip('*')
+print('Cadena modificada:', titulo, len(titulo))
+titulo = '***GlobalMentoring.com.mx***'.rstrip('*')
+print('Cadena modificada:', titulo, len(titulo))
+
+titulo = ' *** GlobalMentoring.com.mx *** '.strip().strip('*').strip() #Quita primero espacios y luego asteriscos
+print('Cadena modificada:', titulo, len(titulo))
