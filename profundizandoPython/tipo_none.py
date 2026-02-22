ceb5f75bd15c5060c592f2ffa93649265cd837ba
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-02-22 12:30:32 -0500

    Video 242 terminado. Tipos None

---
+++ profundizandoPython/tipo_none.py
@@ -0,0 +1,22 @@
+variable = None
+
+#Se considera verdadero si es diferente de None o de cero
+
+if bool(variable):
+    print('Se considera verdadera')
+else:
+    print('Se considera falso')
+
+print(type(variable))
+
+'''
+Ocho formas de generar un booleano False en Python
+1. Comilla simple o doble '' /"" (Cadena vacia)
+2. Lista vacía []
+3. Tupla vacía ()
+4. Diccionario vacío {}
+5. Entero 0
+6. Flotante 0.0
+7. bool False
+8. None
+'''
