de93edd95aa437ac4f3b219f5faaf25c3369c342
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-02-25 17:38:58 -0500

    Video 247 finalizado. Sección 37. Acciones sobre listas

--- profundizandoPython/listas.py
+++ profundizandoPython/listas.py
@@ -5,3 +5,23 @@ nombres1 = ['Juan','Karla','Pedro']
 nombres2 = 'Laura María Gonzalo Ernesto'.split()
 # Sumar listas
 print(f'Sumar listas {nombres1 + nombres2}')
+# Extender una lista con otra lisat
+nombres1.extend(nombres2)
+print(f'Extender la lista1: {nombres1}')
+
+# Lista de números
+numeros1 = [10,40,15,4,20,90,4]
+print(f'Lista original : {numeros1}')
+# Obtener el índice del primer elemento encontrado de una lista
+print(f'ÍNdice 4: {numeros1.index(4)}')
+
+# Invertir el orden de los elementos de una lista
+numeros1.reverse()
+print(f'Lista reversed : {numeros1}')
+
+#Ordenar los elementos de una lista
+numeros1.sort()
+print(f'Lista ordenada ascendente : {numeros1}')
+# Ordenar de manera descendente
+numeros1.sort(reverse=True)
+print(f'Lista ordenada descendente : {numeros1}')
