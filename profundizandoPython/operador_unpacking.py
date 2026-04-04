6c7f79b11ccd023ac0f7dad0c06ea6d4dda52357
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-04-04 15:01:57 -0500

    Sección 38. Video 253. Unpacking en argumentos de python

--- profundizandoPython/operador_unpacking.py
+++ profundizandoPython/operador_unpacking.py
@@ -4,3 +4,9 @@ numeros = [1,2,3]
 print(numeros)
 print(*numeros) #Imprime cada elemento por separado
 print(*numeros, sep=" - ")
+
+
+def sumar(a,b,c):
+    return a+b+c
+
+print(f'Resultado de la suma: {sumar(*numeros)}') #Pasa los parametros inmediatamente
