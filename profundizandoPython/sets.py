0a2ee23efff0240d9c8fb7d06d53fe5e429aa94a
Jorgealza9206 <jaalzar@udistrital.edu.co>
2026-09-08 22:09:41 -0500

    Sección 41. Video 264. Profundizando set en python 2

--- profundizandoPython/sets.py
+++ profundizandoPython/sets.py
@@ -21,3 +21,16 @@ print(conjunto)
 # Crear un set a partir de un iterable
 conjunto = set([1,9,5,8,9]) # Quita el valor duplicado
 print(conjunto)
+# Podemos agregar más elementos o incluso otro set
+conjunto2 = {100, 200, 300, 300}
+conjunto.update(conjunto2) # Agrega los elementos del conjunto2 al conjunto1
+print(conjunto)
+conjunto.update([20,30,40,40]) # Podemos agregar incluso una lista
+print(conjunto)
+
+# Copiar un set (Copia poco profunda, solo copia las referencias)
+conjunto_copia = conjunto.copy()
+print(conjunto_copia)
+# Verificar la igualdad
+print(f'¿Es igual en contenido?: {conjunto == conjunto_copia}')
+print(f'¿Es la misma referencia?: {conjunto is conjunto_copia}')
